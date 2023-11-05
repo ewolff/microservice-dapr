42c71f3438e3e3c5f5a287a8c05217bd75b92dce
Eberhard Wolff <eberhard.wolff@posteo.net>
2023-11-05 09:03:39 +0100

    Remove Apache

--- microservice-dapr-demo/docker-build.sh
+++ microservice-dapr-demo/docker-build.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 docker build --tag=microservice-dapr-postgres:1 postgres
-docker build --tag=microservice-dapr-apache:1 apache
 docker build --tag=microservice-dapr-shipping:1 microservice-dapr-shipping
 docker build --tag=microservice-dapr-invoicing:1 microservice-dapr-invoicing
 docker build --tag=microservice-dapr-order:1 microservice-dapr-order
--- microservice-dapr-demo/docker-push.sh
+++ microservice-dapr-demo/docker-push.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 docker tag microservice-dapr-postgres:1 ewolff/microservice-dapr-postgres:latest
-docker tag microservice-dapr-apache:1 ewolff/microservice-dapr-apache:latest
 docker tag microservice-dapr-shipping:1 ewolff/microservice-dapr-shipping:latest
 docker tag microservice-dapr-invoicing:1 ewolff/microservice-dapr-invoicing:latest
 docker tag microservice-dapr-order:1 ewolff/microservice-dapr-order:latest
-docker push ewolff/microservice-dapr-apache:latest
 docker push ewolff/microservice-dapr-postgres:latest
 docker push ewolff/microservice-dapr-shipping:latest
 docker push ewolff/microservice-dapr-invoicing:latest
